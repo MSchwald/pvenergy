97740727a54140e1521115928317d85d18b2f0ad
MartinSchwald <martin.schwald@uni-due.de>
2025-12-09 04:38:08 +0100

    adapt urls to JS/Django refactoring

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,8 +10,11 @@ MENU = [
 ]
 
 urlpatterns = make_menu_urls(MENU) + [
-    path("weather/<int:system_id>/", views.plot_weather, name="plot_weather"),
-    path("features/<int:system_id>/", views.plot_features, name="plot_features"),
+    path("load-metadata/", views.load_metadata, name="load_metadata"),
     path("load-models/", views.load_models, name="load_models"),
-    path("prediction/<int:system_id>/", views.plot_prediction, name="plot_prediction"),
+    path("load-weather/", views.load_weather, name="load_weather"),
+    path("save-weather/", views.save_weather, name="save_weather"),
+    path("plot-weather/", views.plot_weather, name="plot_weather"),
+    path("plot-features/", views.plot_features, name="plot_features"),
+    path("plot-predictions/", views.plot_predictions, name="plot_predictions"),
 ]
