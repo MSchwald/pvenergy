ba25ec2c9ea493e4c5cfe191345f04d2cd6f0680
MartinSchwald <martin.schwald@uni-due.de>
2025-12-11 16:09:00 +0100

    modularize format settings to convert data to html

---
+++ dashboard/formating.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import pandas as pd
+from pathlib import Path
+
+from feature_processing import FeatureProcessing as fp
+from typing import Any
+
+from django.conf import settings
+
+def number_format(number: float | int | Any) -> str | Any:
+    if isinstance(number, pd.Timestamp):
+        return number.strftime('%X')
+    if not isinstance(number, (float, int)):
+        return number
+    s = str(number)
+    if "." not in s:
+        return s
+    _, mantissa = s.split(".")
+    if len(mantissa) <= 2:
+        return s
+    return f"{round(number, 2):.2f}"
+
+def feature_format(name: str, display_unit: bool = True) -> str:
+    if not isinstance(name, str):
+        return name
+    if not name in fp.ALL_FEATURE_NAMES:
+        return name.replace("_", " ").title()
+    if display_unit:
+        return fp.FEATURE_FROM_NAME[name].display_name_with_unit
+    return fp.FEATURE_FROM_NAME[name].display_name
+
+def pd_styler(data: pd.DataFrame | pd.Series) -> str:
+    """Formats pandas objects with html code for displaying."""
+    df = data.copy()
+    if isinstance(df, pd.Series):
+        df = df.to_frame().T
+    df.columns = [feature_format(col) for col in df.columns]
+    df.columns.name, df.index.name = feature_format(df.index.name), None
+    df_html = df.style.format(
+        formatter=number_format
+    ).format_index(
+        number_format
+    ).to_html(
+        escape=False, table_attributes='class="df-table"'
+    )
+    return f'<div class="table-container">{df_html}</div>'
+
+def static_url(file: Path) -> str:
+    rel = file.relative_to(settings.STATICFILES_DIRS[0])
+    return f"{settings.STATIC_URL}{rel.as_posix()}"
